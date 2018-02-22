09ff29380afb10cf63cb03af7e8ea51d83857f43
Josh Kaplan <kaplan.joshua@gmail.com>
2018-02-22 14:12:54 +0100

    First commit

---
+++ Closest-Beacon/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Closest-Beacon
+//
+//  Created by Josh Kaplan on 2/21/18.
+//  Copyright © 2018 Josh Kaplan. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var locationName: UILabel!
+    
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")! as UUID, identifier: "Estimotes")
+    let colors = [
+        38865: UIColor(red: 46/255, green: 49/255, blue: 146/255, alpha: 1),
+        14477: UIColor(red: 159/255, green: 205/255, blue: 174/255, alpha: 1),
+    ]
+    let label = [
+        38865: "Blueberry Office",
+        14477: "Mint Benoit"
+    ]
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedAlways) {
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+        locationManager.startRangingBeacons(in: region)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.unknown }
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as CLBeacon
+            self.view.backgroundColor = self.colors[closestBeacon.minor.intValue]
+            self.locationName.text = self.label[closestBeacon.minor.intValue]
+        }
+        
+    }
+
+
+}
+

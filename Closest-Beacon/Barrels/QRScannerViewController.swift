1e991b69db3b5e75e6b80ebfcda2e48d5a46a4e7
Josh Kaplan <kaplan.joshua@gmail.com>
2018-04-03 13:45:19 -0600

    Added QR code reader to automatically pull in barrelID

---
+++ Closest-Beacon/Barrels/QRScannerViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ScanQRCodeViewController.swift
+//  Closest-Beacon
+//
+//  Created by Josh Kaplan on 4/3/18.
+//  Copyright © 2018 Josh Kaplan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var mainViewController : MainVC!
+    var qrInputBoxValue : String? = nil
+    
+    var qrCodeFrameView: UIView?
+    var video = AVCaptureVideoPreviewLayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let session = AVCaptureSession()
+        
+        // Define capture device
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+            
+            
+        }
+        catch {
+            print(error)
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        session.startRunning()
+
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects != nil && metadataObjects.count != 0 {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
+                if object.type == AVMetadataObject.ObjectType.qr {
+                    // Extracting the data from QR code and presenting it as a string value
+                    // Set value as input box text
+                    self.mainViewController.setInputBox(text: object.stringValue!)
+                    // Does not work
+                    let alert = UIAlertController(title: "Barrel Identified", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Set", style: .default, handler: { (nil) in
+                        self.dismiss(animated: true, completion: nil)
+                    }))
+                    // Does not work
+                    self.dismiss(animated: true, completion: nil)
+                    
+                }
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
--- Closest-Beacon/Global/Controller/MainVC.swift
+++ Closest-Beacon/Global/Controller/MainVC.swift
@@ -15,18 +15,36 @@ import FirebaseDatabase
 class MainVC: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var locationName: UILabel!
     @IBOutlet weak var loggedInUserEmail: UILabel!
-    
     @IBOutlet weak var barrelIDTF: UITextField!
     
+    var qrInputBoxValue : String? = nil
+    
+    @IBAction func launchQRScanner(_ sender: Any) {
+        let qrScannerViewController = QRScannerViewController()
+        qrScannerViewController.mainViewController = self
+        show(qrScannerViewController, sender: self)
+    }
+    
+    func setInputBox(text: String) {
+        self.barrelIDTF.text = text
+    }
     
     
     var barrelID: String? = ""
     @IBAction func barrelIDButtonOnPressed(_ sender: Any) {
         self.barrelID = barrelIDTF.text
         post()
+        
+        // These next 2 things do not work?
+        self.qrInputBoxValue = nil
+        showAlert(title: "Posted", message: "\(barrelID ?? "") \(locationName)", alertTitle: "Close")
     }
     
-    
+    func showAlert(title: String, message: String, alertTitle: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let uiAlertAction = UIAlertAction(title: alertTitle, style: .default, handler: nil)
+        alert.addAction(uiAlertAction)
+    }
     
     
     // signOutUser
@@ -39,7 +57,7 @@ class MainVC: UIViewController, CLLocationManagerDelegate {
     
     
     let locationManager = CLLocationManager()
-    let region = CLBeaconRegion(proximityUUID: NSUUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")! as UUID, identifier: "Estimotes")
+    let region = CLBeaconRegion(proximityUUID: NSUUID(uuidString: "D0D3FA86-CA76-45EC-9BD9-6AF4F91B4F2A")! as UUID, identifier: "Estimotes")
     let colors = [
         38865: UIColor(red: 46/255, green: 49/255, blue: 146/255, alpha: 1),
         14477: UIColor(red: 159/255, green: 205/255, blue: 174/255, alpha: 1),
@@ -196,11 +214,11 @@ class MainVC: UIViewController, CLLocationManagerDelegate {
         let knownBeacons = beacons.filter{ $0.proximity != CLProximity.unknown }
         if (knownBeacons.count > 0) {
             let closestBeacon = knownBeacons[0] as CLBeacon
-            self.view.backgroundColor = self.colors[closestBeacon.minor.intValue]
+            self.locationName.textColor = self.colors[closestBeacon.minor.intValue]
             self.locationName.text = self.beaconLocation[closestBeacon.minor.intValue]
             if self.currentBeaconID != closestBeacon.minor.intValue {
                 self.currentBeaconID = closestBeacon.minor.intValue
-                self.post()
+//                self.post()
             }
         }
         

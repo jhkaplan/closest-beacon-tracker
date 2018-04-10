9e891cc4ee02ff679baad7bb9c8c3ef6e81f5d7f
mayane mb <mayane.ndoumbe.mbow@enablon.com>
2018-04-10 14:25:12 +0200

    [ splitview ] new video capture implementation

--- Closest-Beacon/Barrels/QRScannerViewController.swift
+++ Closest-Beacon/Barrels/QRScannerViewController.swift
@@ -15,9 +15,6 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
     var mainViewController : MainVC!
     var qrInputBoxValue : String? = nil
     
-    var qrCodeFrameView: UIView?
-    var video = AVCaptureVideoPreviewLayer()
-    
     let greenBoxImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "Green Box")
@@ -26,65 +23,118 @@ class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsD
         return imageView
     }()
     
+    
+    var myView: UIView = UIView()
+    
+    var session: AVCaptureSession!
+    var device: AVCaptureDevice!
+    var input: AVCaptureDeviceInput!
+    var output: AVCaptureMetadataOutput!
+    var prevLayer: AVCaptureVideoPreviewLayer!
+
+    func setMyViewConstraints() {
+        // centrer la container view dans la vue racine
+        self.myView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // set size bounds
+        self.myView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.myView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.myView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.myView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        /*self.qrCodeFrameView.widthAnchor.constraint(equalToConstant: 500).isActive = true
+         self.qrCodeFrameView.heightAnchor.constraint(equalToConstant: 500).isActive = true*/
+        self.myView.backgroundColor = UIColor.yellow
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        qrCodeFrameView = UIView()
-        
-        let session = AVCaptureSession()
-        
-        // Define capture device
-        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
-        
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            session.addInput(input)
-            
-            
-        }
-        catch {
-            print(error)
-        }
-        
-        let output = AVCaptureMetadataOutput()
-        session.addOutput(output)
-        
-        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        
-        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-        
-        video = AVCaptureVideoPreviewLayer(session: session)
-        video.frame = view.layer.bounds
-        view.layer.addSublayer(video)
-        
-        session.startRunning()
-        
-        view.addSubview(greenBoxImageView)
-        view.bringSubview(toFront: greenBoxImageView)
-        setupGreenBoxImageView()
 
+        // create session
+        self.createSession()
+
+        // add my view to root view
+        self.view.addSubview(self.myView)
+        
+        // add constraints
+        self.setMyViewConstraints()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.prevLayer.frame.size = self.myView.frame.size
+    }
+
+    func createSession() {
+
+        // get the capture device
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        // try retrieve
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+
+        self.session = AVCaptureSession()
+        self.session?.addInput(input)
+        self.device = captureDevice
+
+    
+    
+        prevLayer = AVCaptureVideoPreviewLayer(session: session)
+        prevLayer.frame.size = myView.frame.size
+        prevLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        prevLayer.connection?.videoOrientation = transformOrientation(orientation: UIInterfaceOrientation(rawValue: UIApplication.shared.statusBarOrientation.rawValue)!)
+        
+        myView.layer.addSublayer(prevLayer)
+        
+        session.startRunning()
+    }
+    
+    func cameraWithPosition(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        let devices = AVCaptureDevice.devices(for: AVMediaType.video)
+        for device in devices {
+            if device.position == position {
+                return device
+            }
+        }
+        return nil
+    }
+    
+    override func viewWillTransition(to size: CGSize,
+                                     with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { (context) -> Void in
+            self.prevLayer.connection?.videoOrientation = self.transformOrientation(orientation: UIInterfaceOrientation(rawValue: UIApplication.shared.statusBarOrientation.rawValue)!)
+            self.prevLayer.frame.size = self.myView.frame.size
+        }, completion: { (context) -> Void in
+            
+        })
+        super.viewWillTransition(to: size, with: coordinator)
+    }
+    
+    func transformOrientation(orientation: UIInterfaceOrientation) -> AVCaptureVideoOrientation {
+        switch orientation {
+        case .landscapeLeft:
+            return .landscapeLeft
+        case .landscapeRight:
+            return .landscapeRight
+        case .portraitUpsideDown:
+            return .portraitUpsideDown
+        default:
+            return .portrait
+        }
+    }
+
+
     func setupGreenBoxImageView() {
-        greenBoxImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        greenBoxImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        greenBoxImageView.centerXAnchor.constraint(equalTo: self.myView.centerXAnchor).isActive = true
+        greenBoxImageView.centerYAnchor.constraint(equalTo: self.myView.centerYAnchor).isActive = true
         greenBoxImageView.widthAnchor.constraint(equalToConstant: 333).isActive = true
         greenBoxImageView.heightAnchor.constraint(equalToConstant: 333).isActive = true
     }
-    
+
+
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         if metadataObjects != nil && metadataObjects.count != 0 {
             if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
                 if object.type == AVMetadataObject.ObjectType.qr {
-                    // Extracting the data from QR code and presenting it as a string value
-                    // Set value as input box text
                     self.mainViewController.setInputBox(text: object.stringValue!)
-//                    let alert = UIAlertController(title: "Barrel Identified", message: object.stringValue, preferredStyle: .alert)
-//                    alert.addAction(UIAlertAction(title: "Set", style: .default, handler: { (nil) in
-//                        self.dismiss(animated: true, completion: nil)
-//                    }))
-//                    self.present(alert, animated: true)
                     _ = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { (timer) in
                         self.dismiss(animated: true, completion: nil)
                     }
